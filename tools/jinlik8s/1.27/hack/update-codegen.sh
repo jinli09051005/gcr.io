79eecd7971ba176f7060be3a0ff53190a4c7715b
jinli <jinli09051005@163.com>
2024-08-26 09:20:46 +0800

    update tool jinlik8s

--- tools/jinlik8s/1.27/hack/update-codegen.sh
+++ tools/jinlik8s/1.27/hack/update-codegen.sh
@@ -12,8 +12,8 @@ if [ "$1" == "external" ]; then
 bash "${CODEGEN_PKG}"/generate-groups.sh " \
 deepcopy,client,lister,informer" \
  $2/generated/external \
- $2 \
- $3 \
+ $2/$3 \
+ $4 \
  --output-base "${GOPATH}/src" \
  --go-header-file "${SCRIPT}"/boilerplate.go.txt
 fi
@@ -21,17 +21,17 @@ if [ "$1" == "internal" ]; then
 GOPATH="${GOPATH}" bash "${CODEGEN_PKG}"/generate-internal-groups.sh \
 "deepcopy,client,lister,informer,conversion,defaulter" \
  $2/generated/client \
- $2 \
- $2 \
- $3 \
+ $2/$3 \
+ $2/$3 \
+ $4 \
  --output-base "${GOPATH}/src" \
  --go-header-file "${SCRIPT}"/boilerplate.go.txt
 GOPATH="${GOPATH}" bash "${CODEGEN_PKG}"/generate-internal-groups.sh \
 "openapi" \
  $2/generated \
- $2 \
- $2 \
- $3 \
+ $2/$3 \
+ $2/$3 \
+ $4 \
  --output-base "${GOPATH}/src" \
  --go-header-file "${SCRIPT}"/boilerplate.go.txt
 fi
